157ba2d9541a850a4b57a7e3ddd17797f68278d4
VitalyGladyshev <glvv232@mail.ru>
2019-10-25 14:29:47 +0300

    ДЗ 04

--- hh/pipelines.py
+++ hh/pipelines.py
@@ -5,16 +5,37 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-from pymongo import MongoClient
+# from pymongo import MongoClient
+from sqldatabase.database import AvitoRealty
+from sqldatabase.models import Kvart, Photos, Authors, Base
 
 
 class HhPipeline(object):
     def __init__(self):
-        mongo_url = 'mongodb://localhost:27017'
-        client = MongoClient(mongo_url)
-        data_base = client.hh_db
-        self.hh_coll = data_base.hh_coll
+        # mongo_url = 'mongodb://localhost:27017'
+        # client = MongoClient(mongo_url)
+        # data_base = client.hh_db
+        # self.hh_coll = data_base.hh_coll
+        db_url = r'sqlite:///C:\\Projects\\Data_Mining\\avito_realty.sqlite'
+        self.db = AvitoRealty(Base, db_url)
 
     def process_item(self, item, spider):
-        self.hh_coll.insert_one(item)
+        try:
+            av_kv = Kvart(item['name'], item['url'], item['price'], item['address'])
+        except Exception as e:
+            print(e)
+        self.db.session.add(av_kv)
+        self.db.session.commit()
+
         return item
+
+        # author = self.db.session.query(Authors).filter_by(url=item.get('author_url')).first()
+        # if not author:
+        #     author = Authors(item.get('author_url'), item.get('author_name'))
+        #     self.db.session.add(author)
+        #     try:
+        #         self.db.session.commit()
+        #     except Exception as e:
+        #         print(e)
+        #
+        # tags = [Tags(itm["name"], itm["url"]) for itm in item.get('tags')]
--- runner.py
+++ runner.py
@@ -2,12 +2,13 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 
 from hh import settings
-from hh.spiders.hh_search import HhSearchSpider
+# from hh.spiders.hh_search import HhSearchSpider
+from hh.spiders.avito_realty import AvitoRealtySpider
 
 if __name__ == '__main__':
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
 
     process = CrawlerProcess(settings=crawler_settings)
-    process.crawl(HhSearchSpider)     # можно несколько пауков здесь добавить
+    process.crawl(AvitoRealtySpider)     # можно несколько пауков здесь добавить
     process.start()
